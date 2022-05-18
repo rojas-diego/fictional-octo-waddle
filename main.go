8a34c9bc52d4de00975db64a3f0da81331e2e08e
Diego ROJAS <diego@koyeb.com>
2022-05-18 12:34:36 +0200

    feat: cpu metrics

--- main.go
+++ main.go
@@ -1,19 +1,37 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"math/rand"
 	"net/http"
-	"runtime"
 	"strconv"
 	"time"
+
+	"github.com/shirou/gopsutil/cpu"
+)
+
+var commonHttpResponseCode = []int{
+	http.StatusOK,
+	http.StatusCreated,
+	http.StatusBadRequest,
+	http.StatusUnauthorized,
+	http.StatusAccepted,
+	http.StatusNotFound,
+	http.StatusInternalServerError,
+}
+
+var (
+// runtimeInfoLock   sync.Mutex = sync.Mutex{}
+// runtimeInfoLastTS time.Time  = time.Time{}
+// runtimeInfoCpuStats *cpu.Stats = nil
 )
 
 func main() {
 	http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
 		value, err := strconv.Atoi(r.URL.Query().Get("value"))
 		if err != nil {
-			w.WriteHeader(200)
+			w.WriteHeader(commonHttpResponseCode[rand.Intn(len(commonHttpResponseCode))])
 			return
 		}
 		w.WriteHeader(value)
@@ -53,41 +71,83 @@ func main() {
 		time.Sleep(wait)
 	})
 
-	// http.HandleFunc("/compute", func(w http.ResponseWriter, r *http.Request) {
-	// 	duration, err := time.ParseDuration(r.URL.Query().Get("for"))
-	// 	if err != nil {
-	// 		duration = time.Second * 30
-	// 	}
-	// 	go func(duration time.Duration) {
-	// 		ctx, cancel := context.WithTimeout(context.TODO(), duration)
-	// 		defer cancel()
+	http.HandleFunc("/fibonacci", func(w http.ResponseWriter, r *http.Request) {
+		value, err := strconv.Atoi(r.URL.Query().Get("value"))
+		if err != nil {
+			value = 25000
+		}
+		for j := 0; j < value; j++ {
+			a := 0
+			b := 1
+			c := b
+			for {
+				c = b
+				b = a + b
+				if b >= value {
+					break
+				}
+				fmt.Println("value: ", a)
+				a = c
+			}
+		}
+		w.WriteHeader(200)
+	})
 
-	// 		select {
-	// 		case <-ctx.Done():
-	// 		}
-	// 	}(duration)
-	// 	w.WriteHeader(200)
-	// })
+	http.HandleFunc("/runtime_info", func(w http.ResponseWriter, r *http.Request) {
+		// now := time.Now()
+		cpuinfo, err := cpu.Percent(time.Second, false)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		percpuinfo, err := cpu.Percent(time.Second, true)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
 
-	// go func() {
-	// 	for {
-	// 		PrintMemUsage()
-	// 		time.Sleep(time.Second)
-	// 	}
-	// }()
+		// runtimeInfoLock.Lock()
+		// defer runtimeInfoLock.Unlock()
+		// if runtimeInfoCpuStats == nil {
+		// 	w.WriteHeader(http.StatusOK)
+		// 	w.Write([]byte(`"Called for the first time"`))
+		// 	runtimeInfoCpuStats = cpuinfo
+		// 	runtimeInfoLastTS = now
+		// 	return
+		// }
+
+		// deltaNs := now.Sub(runtimeInfoLastTS).Nanoseconds()
+		// cpuDeltaNs := cpuinfo.Total - runtimeInfoCpuStats.Total
+		// totalPercent := (float64(cpuDeltaNs) / float64(deltaNs)) * float64(100)
+
+		// runtimeInfoLastTS = now
+		// runtimeInfoCpuStats = cpuinfo
+
+		type cpuInfoResponse struct {
+			TotalPercent []float64 `json:"total_percent"`
+			PerCpu       []float64 `json:"per_cpu"`
+		}
+
+		body, err := json.MarshalIndent(cpuInfoResponse{
+			TotalPercent: cpuinfo,
+			PerCpu:       percpuinfo,
+		}, "", "    ")
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Write(body)
+
+		// w.WriteHeader(http.StatusOK)
+		// w.Write([]byte(fmt.Sprintf(`{"
+		// 	"total_percent": %f,
+		// 	"total": %d,
+		// 	"delta_ns": %d,
+		// 	"cpu_delta_ns": %d,
+		// "}`, totalPercent, cpuinfo.Total, deltaNs, cpuDeltaNs)))
+	})
+
 	http.ListenAndServe(":3000", nil)
-}
-
-func PrintMemUsage() {
-	var m runtime.MemStats
-	runtime.ReadMemStats(&m)
-	// For info on each, see: https://golang.org/pkg/runtime/#MemStats
-	fmt.Printf("Alloc = %v MiB", bToMb(m.Alloc))
-	fmt.Printf("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
-	fmt.Printf("\tSys = %v MiB", bToMb(m.Sys))
-	fmt.Printf("\tNumGC = %v\n", m.NumGC)
-}
-
-func bToMb(b uint64) uint64 {
-	return b / 1024 / 1024
 }
